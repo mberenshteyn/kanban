2d09667b732a2bcb4ec877db9ae41e2ecdecbf0c
Maxime Berenshteyn <maxime.berenshteyn@gmail.com>
2020-10-30 17:08:41 -0700

    Developed API skeleton

---
+++ api.py
@@ -0,0 +1,21 @@
+def create_user(username, password):
+    pass
+
+def get_user(username, password):
+    pass
+
+def create_board(user, board_name):
+    pass
+
+def get_board(user, board_name):
+    pass
+
+def add_item(user, board, item_name):
+    pass
+
+def update_details(user, board, item_id, new_details):
+    pass
+
+def update_status(user, board, item_id, new_status): 
+    pass
+
