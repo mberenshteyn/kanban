ccf0021cba918d8c09d8299d38d5d832c36a7ab9
Maxime Berenshteyn <maxime.berenshteyn@gmail.com>
2020-11-01 15:16:06 -0800

    Implemented signin and signup functions

--- api.py
+++ api.py
@@ -1,8 +1,61 @@
-def create_user(username, password):
-    pass
+import database
+import bcrypt
+import uuid
+import flask
+from flask import request, jsonify, make_response
 
-def get_user(username, password):
-    pass
+app = flask.Flask(__name__)
+
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    if "username" not in request.args:
+        return "ERROR: user did not supply a username"
+    elif "password" not in request.args:
+        return "ERROR: user did not supply a password"
+
+    username = request.args["username"]
+    password = request.args["password"]
+
+    users = database.connect("users")
+
+    if users.count_documents({"username": username}) != 0:
+        return "ERROR: there is already an account registered under this username!"
+
+    hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    userid = uuid.uuid1()
+    while users.count_documents({"uuid": userid}) != 0:
+        userid = uuid.uuid1()
+
+    new_user = {
+        "uuid": userid,
+        "username": username,
+        "hashed_pwd": hashed
+    }
+
+    users.insert_one(new_user)
+
+@app.route("/signin", methods = ["GET"])
+def signin():
+    if "username" not in request.args:
+        return "ERROR: user did not supply a username"
+    elif "password" not in request.args:
+        return "ERROR: user did not supply a password"
+
+    username = request.args["username"]
+    password = request.args["password"]
+
+    users = database.connect("users")
+    
+    current_user = users.find_one({"username": username})
+    if not current_user:
+        return "ERROR: there is not an account registered under this username!"
+    if not bcrypt.checkpw(password.encode("utf-8"), current_user.get("hashed_pwd")):
+        return "ERROR: username or password is not correct!"
+
+    response = make_response("Generating user cookie")
+    response.set_cookie("uuid", current_user.get("uuid").bytes)
+
+    return response
 
 def create_board(user, board_name):
     pass
